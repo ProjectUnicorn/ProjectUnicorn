2615d899da7a94909cf10859b1555f71ef791397
Jonas Groth Jakobsen <jog@its.aau.dk>
2016-12-22 15:00:22 +0100

    Manglende Import

--- projectunicorn/wsgi-prod.py
+++ projectunicorn/wsgi-prod.py
@@ -8,7 +8,7 @@ https://docs.djangoproject.com/en/1.10/howto/deployment/wsgi/
 """
 
 import os
-
+import sys
 from django.core.wsgi import get_wsgi_application
 
 sys.path.append(os.path.dirname(os.path.abspath(__file__)) + '/..')
