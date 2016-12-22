141e751a2a3cbe0a086571445d1c06b5742e6ec5
Jonas Groth Jakobsen <jog@its.aau.dk>
2016-12-22 15:07:32 +0100

    Flere små ting

--- applicationlist/views.py
+++ applicationlist/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView
+from .tables import *
+from .models import *
 
 # Create your views here.
 class ApplicationListTableView(TemplateView):
