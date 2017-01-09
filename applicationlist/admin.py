6f29c259f16616db2497c9207dabe9f0ba9f0f3a
Jonas Groth Jakobsen <jog@its.aau.dk>
2017-01-09 16:57:00 +0100

    Remember to fix Admin aswell

--- applicationlist/admin.py
+++ applicationlist/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from .models import *
 # Register your models here.
 class ApplicationAdmin(admin.ModelAdmin):
-    list_display = ('applicationId','applicationName','applicationDescription','applicationSystemOwner','applicationOneNoteId')
+    list_display = ('applicationId','applicationName','applicationDescription','applicationSystemOwner')
 
 
 admin.site.register(Application,ApplicationAdmin)
