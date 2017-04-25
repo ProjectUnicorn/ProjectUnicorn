f06d17e6aed45793f0e803293c62ee94264fea20
Simon Holm <sho@its.aau.dk>
2017-04-25 09:33:24 +0200

    Apply MySQL setting recommended by Django to avoid various data failures.

--- projectunicorn/settings-prod.py
+++ projectunicorn/settings-prod.py
@@ -82,6 +82,7 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
         'OPTIONS': {
+            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
             'read_default_file': os.path.join(BASE_DIR, "../MySQL-Credentials.cnf")
         },
 
