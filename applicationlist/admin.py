359315d49668690d4f7a24c1bbb92eea2dcd673b
Søren Balle-Christiansen <srenballe@gmail.com>
2017-05-08 12:32:33 +0200

    Rettelse af admin så den passer til den nye model

--- applicationlist/admin.py
+++ applicationlist/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from .models import *
 # Register your models here.
 class ApplicationAdmin(admin.ModelAdmin):
-    list_display = ('applicationId','applicationName','applicationDescription','applicationSystemOwner')
+    list_display = ('applicationId','applicationName', 'applicationSystem','applicationSystemOwner', 'applicationSystemOwnerContact', 'applicationDescription')
 
 
 admin.site.register(Application,ApplicationAdmin)
